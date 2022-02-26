37c476fa2a353ad18c9617679bdce2583f6c39e1
Nicola Casarin <nico.casarin@icloud.com>
2022-02-26 21:54:58 +0100

    :construction: Initial ConteI SNA

--- src/02_data_preparation.R
+++ src/02_data_preparation.R
@@ -28,7 +28,7 @@ deputies <- deputies %>%
 
 # mutate to interval date format
 deputies <- deputies %>% 
-  mutate(date = s_date %--% e_date)
+  mutate(date = interval(start = s_date, end = e_date))
 
 # creating unique keys for party
 deputies <- deputies %>% 
@@ -84,17 +84,53 @@ contributors <- left_join(
 # if the mp have changed party subset if date_law not included within the political mandate of joint signatory
 contributors <- subset(contributors, (contributors$date_law %within% contributors$date) == TRUE)
 
-#
+# filter unsued columns
 contributors <- contributors %>% 
   select(!c(date_mp, date))
 
+# Conte I data preparation ------------------------------------------------
 
 # Conte I in carica dal 1º giugno 2018 al 5 settembre 2019, 
 # per un totale di 461 giorni, ovvero 1 anno, 3 mesi e 4 giorni. 
 # Dimissioni 20 agosto 2019.
-conteI <- subset(contributors, contributors$date >= "2018-06-01" & contributors$date < "2019-09-05")
+
+# edges governo Conte I
+conteI_edges <- contributors %>% 
+  filter(contributors$date_law >= "2018-06-01" & contributors$date_law < "2019-09-05") %>% 
+  select(joint_signatory, signatory)
+
+# estrazione nodi(deputati) governo Conte I
+deputies_conteI <- deputies %>% 
+  filter(interval(start = "2018-06-01", end = "2019-09-05")  %within% date)
+
+deputies_conteI
+
+conteI_nodes <- unique(conteI_edges$signatory)
+conteI_nodes <- append(conteI_nodes, unique(conteI_edges$joint_signatory))
+conteI_nodes <- tibble(name = unique(conteI_nodes))
+
+conteI_nodes <- left_join(conteI_nodes,
+          subset(deputies, interval(start = "2018-06-01", end = "2019-09-05") %within% deputies$date),
+          by = c("name" = "name"),
+          suffix = c(".conte",".dep")) %>% 
+  select(name, party)
 
 
+# network governo 
+conteI_network <- graph_from_data_frame(conteI_edges, directed = T, vertices = conteI_nodes)
 
+unique(V(conteI_network)$party)
 
+# codifica colori partito
+V(conteI_network)$color <- NA
+V(conteI_network)$color <- ifelse(V(conteI_network)$party == "FRATELLI D'ITALIA ", "#003366","orange")
+V(conteI_network)$color <- ifelse(V(conteI_network)$party == "MOVIMENTO 5 STELLE ", "yellow", V(conteI_network)$color)
+V(conteI_network)$color <- ifelse(V(conteI_network)$party == "LEGA - SALVINI PREMIER ", "forestgreen", V(conteI_network)$color)
+V(conteI_network)$color <- ifelse(V(conteI_network)$party == "FORZA ITALIA - BERLUSCONI PRESIDENTE ", "lightblue", V(conteI_network)$color)
+V(conteI_network)$color <- ifelse(V(conteI_network)$party == "PARTITO DEMOCRATICO ", "red", V(conteI_network)$color)
+V(conteI_network)$color <- ifelse(V(conteI_network)$party == "LIBERI E UGUALI ", "magenta", V(conteI_network)$color)
+V(conteI_network)$color <- ifelse(V(conteI_network)$party == "MISTO ", "peachpuff4", V(conteI_network)$color)
+
+# plot network
+plot(conteI_network, vertex.size = 10, vertex.label = NA,vertex.label.cex = .7,edge.arrow.size = .3,edge.width = .7)
 
