79e9666eaae5e244031c5636a515c6711a710930
Nicola Casarin <nico.casarin@icloud.com>
2022-03-04 14:57:52 +0100

    :bulb: 02_data_preparation source code comments

--- src/02_data_preparation.R
+++ src/02_data_preparation.R
@@ -1,120 +1,130 @@
 # Deputies data preparation -----------------------------------------------
 
-# import deputies.csv, skipping the first non-interesting variable
+# import deputies.csv
+deputies <- vroom(here("data/deputies.csv"),
+                  #skipping the non used data
+                  col_select = c("name", "partito", "s_office", "e_office"))
 
-deputies <- vroom(here('data/deputies.csv'),
-      col_select = c('name','partito',"s_office","e_office"))
-
-deputies <- deputies %>% 
+deputies <- deputies %>%
+  # removing the office term included in the party var
   separate(col= partito,
-           into = c('party','trash'),
-           sep= '\\(') %>% 
-  select(!(trash))
-deputies$party <- str_trim(deputies$party) 
+           into = c("party","trash"),
+           sep= "\\(") %>%
+  select(!(trash)) %>%
+  mutate(party = str_trim(party))
 
-# parsing party dates
 
-deputies$e_office <- ifelse(test = is.na(deputies$e_office), yes = 20220228, no = deputies$e_office)
+# Parsing offices data vars
+deputies$e_office <- ifelse(test = is.na(deputies$e_office),
+                            yes = 20220228, 
+                            no = deputies$e_office)
 
-deputies <- deputies %>% 
+deputies <- deputies %>%
   mutate(s_office = ymd(s_office),
          e_office = ymd(e_office))
 
-# mutate to interval date format
-
-deputies <- deputies %>% 
-  mutate(date = interval(start = s_office, end = e_office)) %>% 
+# mutating dates into interval format
+deputies <- deputies %>%
+  mutate(date = interval(start = s_office, end = e_office)) %>%
   select(!c(s_office, e_office))
 
-# drop deputies which have taken the office AFTER the Conte II end
-
-deputies <- deputies %>% 
+# drop deputies which have taken office AFTER the ConteII int. end:
+deputies <- deputies %>%
   filter(int_start(date) < ymd("2021-12-02"))
 
 # Conte I data preparation ------------------------------------------------
 
-# import contributors.csv, skipping the first non-interesting variable
-
+# import contributors.csv:
 conteI <- vroom(here("data/conteI.csv"),
-                      col_select = c('num':'joint_signatory'))
+                # skipping the first non-interesting variable
+                col_select = c("num":"joint_signatory"))
 
-# Due to the construction of the semantic dataframe it is more easy removing non unique obs with R
+# Due to the construction of dati.camera.it database it is easier preparing data
+# within R environment:
 
+# removing ducplicate edges
 conteI <- unique(conteI)
 
-# Conte I date interval
-
+# ConteI cabinet dates
 conteI_date <- interval(ydm("2018-31-05"), ydm("2019-04-09"))
 
-# Parsing date
-
+# Parsing dates
 conteI$date <- ymd(conteI$date)
 
-# Extracting Conte I deputies and contributors e dealing with duplicate nodes
-
-conteI_deputies <- deputies %>% 
-  filter(int_start(date) < int_end(conteI_date)) %>% 
+# Extracting Conte I deputies and contributors
+conteI_deputies <- deputies %>%
+  filter(int_start(date) < int_end(conteI_date)) %>%
   filter(!(int_end(date) < int_start(conteI_date)))
 
-duplicate <- conteI_deputies %>% 
-  group_by(name) %>% 
-  summarise(n = n()) %>% 
-  filter(n >1) %>% 
-  add_column(party = "SWITCHER/DECAYED") %>% 
+# Some MPs, who have changed party or decayed during Conte I cabinet
+# are coded as SWITCHER/DECAYED
+duplicate <- conteI_deputies %>%
+  group_by(name) %>%
+  summarise(n = n()) %>%
+  filter(n >1) %>%
+  add_column(party = "SWITCHER/DECAYED") %>%
   select(name, party)
-conteI_deputies <- conteI_deputies %>% 
-  anti_join(duplicate, by = "name") %>% 
+  #after selecting non unique MPs we remove them with
+  # an anti joint from the deputies df
+conteI_deputies <- conteI_deputies %>%
+  anti_join(duplicate, by = "name") %>%
   select(!(date))
+  # lastly we append again the Switcher with the recoded party id
 conteI_deputies <- rbind(conteI_deputies, duplicate)
 
-#remove orphan nodes 
-conteI_deputies <- conteI_deputies[-(which(conteI_deputies$name %!in% conteI$signatory & conteI_deputies$name %!in% conteI$joint_signatory)),]
+#remove orphan nodes
+conteI_deputies <- conteI_deputies[
+  - (which(conteI_deputies$name %!in% conteI$signatory &
+  conteI_deputies$name %!in% conteI$joint_signatory)), ]
 
-# debug deputies list
-
+# debug to check eventual non represented edges
 table(unique(conteI$signatory) %in% conteI_deputies$name)
 table(unique(conteI$joint_signatory) %in% conteI_deputies$name)
 
-# since there one of the deputies is no included on the nodes df we must debug manually!
-
+# since there is MPs no included in the nodes df we must debug it!
 index <- which(!(conteI$joint_signatory %in% conteI_deputies$name))
 index
-debug <- conteI[index,]
+debug <- conteI[index, ]
 debug <- unique(debug$joint_signatory)
-conteI_deputies <- rbind(conteI_deputies, deputies[deputies$name %in% debug,1:2])
+conteI_deputies <- rbind(conteI_deputies, 
+                         deputies[deputies$name %in% debug,1:2])
 
-# Conte II data preparation --------------------------------------------------
+# Conte II data preparation ---------------------------------------------------
 
-# import contributors.csv, skipping the first non-interesting variable
+# Import contributions during Conte II cabinets
 conteII <- vroom(here("data/conteII.csv"),
-                 col_select = c('num':'joint_signatory'))
+                #skipping the non-interesting variable
+                 col_select = c("num":"joint_signatory"))
 
+# removing ducplicate edges
 conteII <- unique(conteII)
 
-# Conte II date interval
+# Conte II cabinet interval
 conteII_date <- interval(ydm("2019-04-09"), ydm("2021-12-02"))
 
-# Parsing date
+# Parsing interval dates
 conteII$date <- ymd(conteII$date)
 
-# Extracting Conte II deputies and contributors
-conteII_deputies <-  deputies %>% 
-  filter(int_start(date) < int_end(conteII_date)) %>% 
+# Extracting Conte II MPs
+conteII_deputies <-  deputies %>%
+  filter(int_start(date) < int_end(conteII_date)) %>%
   filter(!(int_end(date) < int_start(conteII_date)))
 
-# Extracting Conte II deputies and contributors e dealing with duplicate nodes
-duplicate <- conteII_deputies %>% 
-  group_by(name) %>% 
-  summarise(n = n()) %>% 
-  filter(n >1) %>% 
-  add_column(party = "SWITCHER/DECAYED") %>% 
+# Dropping Conte II deputies and contributors e dealing with duplicate nodes
+duplicate <- conteII_deputies %>%
+  group_by(name) %>%
+  summarise(n = n()) %>%
+  filter(n >1) %>%
+  add_column(party = "SWITCHER/DECAYED") %>%
   select(name, party)
-conteII_deputies <- conteII_deputies %>% 
-  anti_join(duplicate, by = "name") %>% 
+  #again: after selecting non unique MPs we remove them with
+  # an anti joint from the deputies df
+conteII_deputies <- conteII_deputies %>%
+  anti_join(duplicate, by = "name") %>%
   select(!(date))
 conteII_deputies <- rbind(conteII_deputies, duplicate)
 
-#remove orphan nodes 
+#remove orphan nodes, if presents
 conteII_deputies <- conteII_deputies[-(which(conteII_deputies$name %!in% conteII$signatory & conteII_deputies$name %!in% conteII$joint_signatory)),]
 
 
@@ -125,22 +135,24 @@ table(unique(conteII$joint_signatory) %in% conteII_deputies$name)
 # manual inspection:
 index <- which(!(conteII$joint_signatory %in% conteII_deputies$name))
 index
-debug <- conteII[index,]
+debug <- conteII[index, ]
 debug <- unique(debug$joint_signatory)
-# the inspection revealed that the missing MPs are those who took the first term 
-# office AFTER the start of Conte II! Since that, we will add them to the nodes df
+# the inspection revealed that the missing MPs
+# are those who took the first term office AFTER the start of Conte II!
+# Since that, we will add them to the nodes df
 conteII_deputies <- rbind(conteII_deputies, deputies[deputies$name %in% debug,1:2])
 
 # IV pol. group was founded only 10 days after ConteII starting date. The script
 # codes IV MPs as switcher. Recognizing the weight of IV MPs we must recode them manually
-iv <- deputies %>% 
-  filter(party == "ITALIA VIVA") %>% 
-  filter(int_start(date) == ymd("2019-09-19") & int_end(date) > int_end(conteII_date)) %>% 
+iv <- deputies %>%
+  filter(party == "ITALIA VIVA") %>%
+  filter(int_start(date) == ymd("2019-09-19") &
+        int_end(date) > int_end(conteII_date)) %>%
   select(!(date))
 
-conteII_deputies$party <- ifelse(conteII_deputies$name %in% iv$name, "ITALIA VIVA", conteII_deputies$party)
+conteII_deputies$party <- ifelse(conteII_deputies$name %in% iv$name,
+                                "ITALIA VIVA", 
+                                conteII_deputies$party)
 
-# Removing tmp variables --------------------------------------------------
-
+# Removing tmp/working variables --------------------------------------------------
 rm(conteI_date, conteII_date, debug, index, deputies, duplicate, iv)
-
